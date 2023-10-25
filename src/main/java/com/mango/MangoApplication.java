064708b5bad02d73472ed4a34635e1b46543cf8d
leehaesong3966 <leehaesong3966@naver.com>
2023-10-25 14:25:52 +0900

    setting : 기초 세팅

--- src/main/java/com/mango/MangoApplication.java
+++ src/main/java/com/mango/MangoApplication.java
@@ -1,4 +1,4 @@
-package com.mango.mango;
+package com.mango;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mango/MangoApplicationTests.java
+++ src/test/java/com/mango/MangoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mango.mango;
+package com.mango;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
