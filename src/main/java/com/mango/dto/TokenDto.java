b51dfe467f3ff89681fb0016597ec8d7bc7a12e3
kimYejin <kimphoby0125@gmail.com>
2023-11-05 20:33:44 +0900

    로그인,회원가입

--- src/main/java/com/mango/dto/TokenDto.java
+++ src/main/java/com/mango/dto/TokenDto.java
@@ -1,15 +1,12 @@
 package com.mango.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
-@AllArgsConstructor
-@Builder
+@Setter
+@NoArgsConstructor
 public class TokenDto {
     private String grantType;
     private String accessToken;
-    private String refreshToken;
     private String accessTokenExpiresIn;
 }
--- src/main/java/com/mango/entity/User.java
+++ src/main/java/com/mango/entity/User.java
@@ -1,21 +1,23 @@
 package com.mango.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
 @Table(name = "User")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
@@ -31,5 +33,38 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<SearchLog> searchLogList;
 
+    @Builder
+    public User(Long id, String username, String password, List<ReviewLike> reviewLikeList, List<Restaurant> restaurantList, List<SearchLog> searchLogList) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.reviewLikeList = reviewLikeList;
+        this.restaurantList = restaurantList;
+        this.searchLogList = searchLogList;
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
