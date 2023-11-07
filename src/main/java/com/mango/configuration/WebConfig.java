d0fffc6f9dc56d1351e6055afb63e0d2f9178e3d
kimYejin <kimphoby0125@gmail.com>
2023-11-07 12:55:52 +0900

    fix cors

--- src/main/java/com/mango/configuration/WebConfig.java
+++ src/main/java/com/mango/configuration/WebConfig.java
@@ -13,10 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://34.83.15.61:8080")
+                .allowedOrigins("http://localhost:3000", "http://34.83.15.61:3000")
                 .allowedHeaders("Content-Type", "X-AUTH-TOKEN", "Authorization", "Bearer")
                 .allowedMethods(HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.OPTIONS.name())
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .exposedHeaders("Authorization", "X-AUTH-TOKEN", "Bearer")
                 .maxAge(3600);
     }
